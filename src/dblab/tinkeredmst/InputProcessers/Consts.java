7de0b3ef56ac19ec6b54cdb9eda96aaaa80083d5
kimminkwon <kmk130519@gmail.com>
2020-11-24 14:34:43 +0900

    Initial Const static class in InputProcesser
    201124

---
+++ src/dblab/tinkeredmst/InputProcessers/Consts.java
@@ -0,0 +1,7 @@
+package dblab.tinkeredmst.InputProcessers;
+
+public class Consts {
+    public final static int NUMOFTERMINALS = 1000;
+    public final static int NUMOFPARTITIONS = 20;
+    public final static int NUMOFPORTALS = 5;
+}
