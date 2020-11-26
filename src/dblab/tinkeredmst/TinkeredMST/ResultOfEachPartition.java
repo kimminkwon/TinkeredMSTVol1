93e223418829c9095fe2c570caa8a3d427221588
kimminkwon <kmk130519@gmail.com>
2020-11-26 19:11:01 +0900

    save result for partition: using length, time
    201126

---
+++ src/dblab/tinkeredmst/TinkeredMST/ResultOfEachPartition.java
@@ -0,0 +1,43 @@
+package dblab.tinkeredmst.TinkeredMST;
+
+import dblab.tinkeredmst.InputProcessers.Partition;
+
+public class ResultOfEachPartition {
+    private int partitionNumber;
+    private Partition partitionInfo;
+    private double usingLength;
+    private double time;
+
+    public ResultOfEachPartition(Partition partitionInfo, double[] results) {
+        this.partitionNumber = partitionInfo.getNumber();
+        this.partitionInfo = partitionInfo;
+        this.time = results[0];
+        this.usingLength = results[1];
+    }
+
+    public int getPartitionNumber() {
+        return partitionNumber;
+    }
+
+    public Partition getPartitionInfo() {
+        return partitionInfo;
+    }
+
+    public double getUsingLength() {
+        return usingLength;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "ResultOfEachPartition{" +
+                "partitionNumber=" + partitionNumber +
+                ", partitionInfo=" + partitionInfo +
+                ", usingLength=" + usingLength +
+                ", time=" + time +
+                '}';
+    }
+}
