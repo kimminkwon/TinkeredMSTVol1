441fc39de2f4b2dc0d7efdf438b75d3c48fc74d6
kimminkwon <kmk130519@gmail.com>
2020-11-30 17:30:22 +0900

    1) save distance of each partitions 2) make MST each partitions 3) make totalResult
    201130

--- src/dblab/tinkeredmst/Main.java
+++ src/dblab/tinkeredmst/Main.java
@@ -4,11 +4,10 @@ import dblab.tinkeredmst.BenchmarkModel.ResultOfBenchmarkModel;
 import dblab.tinkeredmst.InputProcessers.*;
 import dblab.tinkeredmst.MST.MST;
 import dblab.tinkeredmst.MST.MST_UsingDistArr;
-import dblab.tinkeredmst.TinkeredMST.CalculateForDistanceTwoPartitions;
-import dblab.tinkeredmst.TinkeredMST.DistanceOfEachPartitions;
-import dblab.tinkeredmst.TinkeredMST.ResultOfEachPartition;
-import dblab.tinkeredmst.TinkeredMST.ResultsOfEachPartitions;
+import dblab.tinkeredmst.Results.CompareTwoResults;
+import dblab.tinkeredmst.TinkeredMST.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -25,22 +24,69 @@ public class Main {
         // 3. 각 Partition 별로 Sub MST를 만든다.
         ResultsOfEachPartitions resultsOfEachPartition = makeResultsOfEachPartitions(inputProceeser.getPartitionStatus());
 
-        // ============================== 현재까지 값 확인 ============================== //
-        printForCheck1(resultOfBenchmarkModel, resultsOfEachPartition);
-        // =========================================================================== //
+            long start = System.currentTimeMillis();
 
         // 4. 각 Partition 별 거리를 저장한다.
         DistanceOfEachPartitions distanceOfEachPartitions = makeDistanceOfEachPartitions(inputProceeser);
 
+        // 5. Partition 사이 MST를 구축한다.
+        double totalLengthOfEachPartitions = makeTotalLengthOfEachPartitions(resultsOfEachPartition);
+        double connectingLengthOfTinkeredMST = makeResultOfTinkeredMST(distanceOfEachPartitions);
 
+            long end = System.currentTimeMillis();
+            double timeOfTinkeredMST = (end - start) / 1000.0;
 
+        printForCheck3(resultOfBenchmarkModel, totalLengthOfEachPartitions, connectingLengthOfTinkeredMST);
+        ResultOfTinkeredMST resultOfTinkeredMST = makeResultOfTinkeredMST(timeOfTinkeredMST, totalLengthOfEachPartitions + connectingLengthOfTinkeredMST);
+
+        // 6. Benchmark model과 TinkeredMST의 Result를 통합관리
+        CompareTwoResults compareTwoResults = new CompareTwoResults(resultOfBenchmarkModel, resultOfTinkeredMST);
+        printForCheck4(compareTwoResults);
+    }
+
+    private static void printForCheck4(CompareTwoResults compareTwoResults) {
+        System.out.println();
+        System.out.println("============================== RESULT ==============================");
+        System.out.println("* RESULT OF TIME: " + compareTwoResults.getResultOfTime());
+        System.out.println("* RESULT OF LENGTH: " + compareTwoResults.getResultOfLength());
+    }
+
+    private static ResultOfTinkeredMST makeResultOfTinkeredMST(double timeOfTinkeredMST, double finalResultLengthForTinkeredMST) {
+        return new ResultOfTinkeredMST(timeOfTinkeredMST, finalResultLengthForTinkeredMST);
+    }
+
+    private static void printForCheck3(ResultOfBenchmarkModel resultOfBenchmarkModel, double totalLengthOfEachPartitions, double connectingLengthOfTinkeredMST) {
+        System.out.println("* length of benchmarkModel: " + resultOfBenchmarkModel.getUsingLength());
+        System.out.println("* Final result length for Tinkered MST: " + (connectingLengthOfTinkeredMST + totalLengthOfEachPartitions));
+        System.out.println("    - Total length for partitions(not connecting each partitions): " + totalLengthOfEachPartitions);
+        System.out.println("    - Connecting length for partitions: " + connectingLengthOfTinkeredMST);
+        System.out.println("* Ratio for two results: " + ((connectingLengthOfTinkeredMST + totalLengthOfEachPartitions) / resultOfBenchmarkModel.getUsingLength() * 100.0));
+    }
+
+    private static double makeTotalLengthOfEachPartitions(ResultsOfEachPartitions resultsOfEachPartition) {
+        double totalLengthOfEachPartitions = 0;
+        for (int i = 0; i < Consts.NUMOFPARTITIONS; i++) {
+            totalLengthOfEachPartitions += resultsOfEachPartition.getPartitionResultMap(i).getUsingLength();
+        }
+        return totalLengthOfEachPartitions;
+    }
+
+    private static double makeResultOfTinkeredMST(DistanceOfEachPartitions distanceOfEachPartitions) {
+        MST mst = new MST_UsingDistArr(distanceOfEachPartitions.getDistanceOfEachPartitions());
+        return mst.getMSTResult()[1];
+    }
+
+    private static void printForCheck2(DistanceOfEachPartitions distanceOfEachPartitions) {
+        for(int i = 0; i < Consts.NUMOFPARTITIONS; i++) {
+            System.out.println(Arrays.toString(distanceOfEachPartitions.getDistanceOfEachPartitions()[i]));
+        }
     }
 
     private static DistanceOfEachPartitions makeDistanceOfEachPartitions(InputProceeser inputProceeser) {
         DistanceOfEachPartitions distanceOfEachPartitions = new DistanceOfEachPartitions();
         for(Partition p : inputProceeser.getPartitionStatus()) {
             for(AdjPartition adjP : p.getAdjPartitions()) {
-                if(distanceOfEachPartitions.getDistanceOfTwoPartition(p.getNumber(), adjP.getNumber()) == 0) {
+                if(distanceOfEachPartitions.getDistanceOfTwoPartition(p.getNumber(), adjP.getNumber()) == 0.0) {
                     CalculateForDistanceTwoPartitions distanceTwoPartitions = new CalculateForDistanceTwoPartitions(p, inputProceeser.getPartitionStatus().get(adjP.getNumber()), adjP);
                     double distance = distanceTwoPartitions.getDistance();
                     distanceOfEachPartitions.setDistanceOfTwoPartition(p.getNumber(), adjP.getNumber(), distance);
@@ -76,17 +122,4 @@ public class Main {
 
         return inputProceeser;
     }
-
-    private static void printForCheck1(ResultOfBenchmarkModel resultOfBenchmarkModel, ResultsOfEachPartitions resultsOfEachPartition) {
-        double box = 0;
-        for (int i = 0; i < Consts.NUMOFPARTITIONS; i++) {
-            box += resultsOfEachPartition.getPartitionResultMap(i).getUsingLength();
-        }
-
-        System.out.println("* length of benchmarkModel: " + resultOfBenchmarkModel.getUsingLength());
-        System.out.println("* Total length for partitions(not connecting each partitions): " + box);
-        System.out.println("* Ratio of benchmark & total length for partitions: " + ( (box / resultOfBenchmarkModel.getUsingLength()) * 100.0 ));
-    }
-
-
 }
