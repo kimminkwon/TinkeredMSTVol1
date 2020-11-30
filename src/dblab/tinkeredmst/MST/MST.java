f2c9666bccd6642301bf664908ae27f28a54a17f
kimminkwon <kmk130519@gmail.com>
2020-11-30 17:31:22 +0900

    error fix: in selecting edge, didnt selected zero(no edge)
    201130

--- src/dblab/tinkeredmst/MST/MST.java
+++ src/dblab/tinkeredmst/MST/MST.java
@@ -7,13 +7,6 @@ public abstract class MST {
         long start = System.currentTimeMillis();
         double resultOfLength = makeMST();
 
-        // 시간 측정 유효성 검사
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         long end = System.currentTimeMillis();
 
         // 0: 실행시간, 1: 사용한 길이
