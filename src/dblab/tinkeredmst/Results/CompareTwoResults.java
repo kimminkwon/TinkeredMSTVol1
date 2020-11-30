16ec62323e664befb330013175deb556dddded4a
kimminkwon <kmk130519@gmail.com>
2020-11-30 17:28:47 +0900

    add constructor
    201130

--- src/dblab/tinkeredmst/Results/CompareTwoResults.java
+++ src/dblab/tinkeredmst/Results/CompareTwoResults.java
@@ -7,6 +7,11 @@ public class CompareTwoResults {
     private ResultOfBenchmarkModel benchmarkModel;
     private ResultOfTinkeredMST tinkeredMST;
 
+    public CompareTwoResults(ResultOfBenchmarkModel benchmarkModel, ResultOfTinkeredMST tinkeredMST) {
+        this.benchmarkModel = benchmarkModel;
+        this.tinkeredMST = tinkeredMST;
+    }
+
     // 반환은 백분율로: 기댓값 100%보다 크지만 100%에 근접
     public double getResultOfLength() {
         return (tinkeredMST.getUsingLength() / benchmarkModel.getUsingLength()) * 100.0;
