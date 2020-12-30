3b67643d9a8dbf200a8c76775de792a5b7fd83c2
kimminkwon <kmk130519@gmail.com>
2020-12-30 18:09:20 +0900

    modify class: add setter & set not final
    201230

--- src/dblab/tinkeredmst/InputProcessers/Consts.java
+++ src/dblab/tinkeredmst/InputProcessers/Consts.java
@@ -1,7 +1,24 @@
 package dblab.tinkeredmst.InputProcessers;
 
 public class Consts {
-    public final static int NUMOFTERMINALS = 1000;
-    public final static int NUMOFPARTITIONS = 20;
-    public final static int NUMOFPORTALS = 5;
+    public static int NUMOFTERMINALS = 50000;
+    public static int NUMOFPARTITIONS = 12;
+    public static int NUMOFPORTALS = 5;
+    public static String FILENAME = "";
+
+    public static void setFILENAME(String FILENAME) {
+        Consts.FILENAME = FILENAME;
+    }
+
+    public static void setNUMOFTERMINALS(int NUMOFTERMINALS) {
+        Consts.NUMOFTERMINALS = NUMOFTERMINALS;
+    }
+
+    public static void setNUMOFPARTITIONS(int NUMOFPARTITIONS) {
+        Consts.NUMOFPARTITIONS = NUMOFPARTITIONS;
+    }
+
+    public static void setNUMOFPORTALS(int NUMOFPORTALS) {
+        Consts.NUMOFPORTALS = NUMOFPORTALS;
+    }
 }
